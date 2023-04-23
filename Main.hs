945326b5cc2df648dbf1e98122a7e04767058e3c
player01osu <player01osu@gmail.com>
2023-04-23 17:44:25 -0400

    Use >>= binding instead of unnessessary do

--- Main.hs
+++ Main.hs
@@ -2,8 +2,7 @@ import System.Random
 import Text.Read
 
 game_loop :: Int -> IO ()
-game_loop answer = do
-   n <- getLine
+game_loop answer = getLine >>= \n ->
    case readMaybe n :: Maybe Int of
       Just guess -> case compare guess answer of
             LT -> putStrLn "Too low" >> game_loop answer
@@ -11,6 +10,4 @@ game_loop answer = do
             otherwise -> putStrLn "That's it"
       Nothing -> putStrLn "Must be a number" >> game_loop answer
 
-main = do
-   n <- (randomIO :: IO Int)
-   putStrLn "Guess a number between 1-100." >> game_loop (mod n 100)
+main = (randomIO :: IO Int) >>= \n -> putStrLn "Guess a number between 1-100." >> game_loop (mod n 100)
