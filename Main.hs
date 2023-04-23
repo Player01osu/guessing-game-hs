1fcc04e02884037f79cc360c032e2549a9d6b7fd
player01osu <player01osu@gmail.com>
2023-04-23 17:50:31 -0400

    Replace otherwise w/ EQ

--- Main.hs
+++ Main.hs
@@ -7,7 +7,7 @@ game_loop answer = getLine >>= \n ->
       Just guess -> case compare guess answer of
             LT -> putStrLn "Too low" >> game_loop answer
             GT -> putStrLn "Too high" >> game_loop answer
-            otherwise -> putStrLn "That's it"
+            EQ -> putStrLn "That's it"
       Nothing -> putStrLn "Must be a number" >> game_loop answer
 
 main = (randomIO :: IO Int) >>= \n -> putStrLn "Guess a number between 1-100." >> game_loop (mod n 100)
